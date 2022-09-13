625a0e6e8d9d94fc90e91de00c7bae2e489158c3
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-09-13 16:53:57 +0200

    ComicController

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comic;
 
 class ComicsController extends Controller
 {
-    //
+    public function index(){
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
+    }
 }
