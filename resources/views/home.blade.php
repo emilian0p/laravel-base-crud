56753e02d04fa009546e465d6ecb366bf220f95d
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-09-13 16:48:41 +0200

    controller, seeder, migration and blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts\main')
+
+@section('main_content')
+
+    <h1>HOMEPAGE</h1>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">I nostri fumetti</a></li>
+                <li><a href="#">Inserisci un nuovo fumetto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    @yield('main_content')
+</body>
+</html>
