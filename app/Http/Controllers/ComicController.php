c58b07ec5cce1ccaa413bcbe26e51c5b42be55aa
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-09-14 18:15:46 +0200

    problems with db

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -5,10 +5,51 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Comic;
 
-class ComicsController extends Controller
+class ComicController extends Controller
 {
-    public function index(){
-        $comics = Comic::all();
-        return view('/comics.index', compact('comics'));
+
+    public function index()
+    {
+        $comics_array = Comic::all();
+
+        $data = [
+            'comics_array' => $comics_array
+        ];
+        return view('comics.index', $data);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
     }
 }
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>I nostri fumetti</h1>
-    @foreach ($comics as $comic)
+    <h1>I fumetti</h1>
+    @foreach ($comics_array as $comic)
         <div>
-            <a href="{{ route('comics.show', [$comic->id]) }}">
+            <a href="{{ route('comics.show', [$comic['id']]) }}">
+                <img src="{{ $comic['thumb'] }}" alt="" width="200px">
                 <div>
-                    <div>Titolo: {{ $comic->title }}</div>
-                    <div>Serie: {{ $comic->series }}</div>
-                    <div>Tipologia: {{ $comic->type }}</div>
-                    <div>Prezzo: {{ $comic->price }}</div>
+                    <div>Titolo: {{ $comic['title'] }}</div>
+                    <div>Descrizione: {{ $comic['description'] }}</div>
+                    <div>Copertina: {{ $comic['thumb'] }}</div>
+                    <div>Prezzo: {{ $comic['price'] }}</div>
+                    <div>Serie: {{ $comic['series'] }}</div>
+                    <div>Data Sconto: {{ $comic['sale_date'] }}</div>
+                    <div>Tipologia: {{ $comic['type'] }}</div>
                 </div>
             </a>
         </div>
